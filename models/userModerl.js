ec0c1f2ffe15657bc129b0db51c42173060f7199
Mohamed Fazil M <fazilmm860@gmail.com>
2023-03-25 14:12:58 +0530

    User Schema model created

---
+++ models/userModerl.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please tell your name']
+
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide your email'],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'Please provide a valid email id']
+
+
+    },
+    photo: {
+        type: String,
+
+    },
+    Password: {
+        type: String,
+        required: [true, ' Please provid a password'],
+        minlength: 8
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, 'Please confirm your Password']
+    }
+});
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
