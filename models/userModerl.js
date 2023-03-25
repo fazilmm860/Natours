88b88660b9cc64bcfa76ddcfd99d1e76b30e4696
Mohamed Fazil M <fazilmm860@gmail.com>
2023-03-25 17:17:40 +0530

    password Encryption using npm bcrypt package

--- models/userModerl.js
+++ models/userModerl.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -20,16 +21,35 @@ const userSchema = new mongoose.Schema({
         type: String,
 
     },
-    Password: {
+    password: {
         type: String,
         required: [true, ' Please provid a password'],
         minlength: 8
     },
     passwordConfirm: {
         type: String,
-        required: [true, 'Please confirm your Password']
+        required: [true, 'Please confirm your Password'],
+        //This only works on SAVE!
+        validate: {
+            validator: function (el) {
+                return el === this.password;
+            },
+            message: 'Password are not the same!'
+        }
     }
 });
+
+userSchema.pre('save', async function (next) {
+    //Only run this function  if password was actually modified
+    if (!this.isModified('password')) return next();
+
+    //Hash the password with cost of 12
+    this.password = await bcrypt.hash(this.password, 12);
+
+    //Delete passwordConfirm field
+    this.passwordConfirm = undefined;
+    next();
+})
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
