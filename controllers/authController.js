85c374247c88e585f106918cc9ef7f7666771c08
Mohamed Fazil M <fazilmm860@gmail.com>
2023-03-29 16:20:25 +0530

    Logging user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,14 @@
 const jwt = require('jsonwebtoken');
-const User = require('./../models/userModerl')
+const User = require('../models/userModel')
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
+
+const signToken = id => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
+    });
+}
 exports.signup = catchAsync(async (req, res, next) => {
     const newUser = await User.create({
         name: req.body.name,
@@ -9,9 +16,7 @@ exports.signup = catchAsync(async (req, res, next) => {
         password: req.body.password,
         passwordConfirm: req.body.passwordConfirm
     });
-    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES_IN
-    })
+    const token = signToken(newUser._id);
 
     res.status(201).json({
         status: 'success',
@@ -21,3 +26,25 @@ exports.signup = catchAsync(async (req, res, next) => {
         }
     });
 });
+exports.login = catchAsync(async (req, res, next) => {
+    const { email, password } = req.body;
+    // 1) Check if email and password exist
+    if (!email || !password) {
+        return next(new AppError('please provide email and password', 400));
+    }
+    // 2) Check if user exist && password is correct
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user || !(await user.correctPassword(password, user.password))) {
+        return next(new AppError('Incorrect email or Password', 401));
+    }
+    // 3) if everthing ok, send token to client
+    const token = signToken(user._id);
+    res.status(200).json({
+        status: 'success',
+        token
+    });
+});
+// exports.protect = catchAsync((req, res, next) => {
+//     next();
+// })
--- models/userModel.js
+++ models/userModel.js
@@ -24,7 +24,8 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: [true, ' Please provid a password'],
-        minlength: 8
+        minlength: 8,
+        select: false
     },
     passwordConfirm: {
         type: String,
@@ -49,7 +50,12 @@ userSchema.pre('save', async function (next) {
     //Delete passwordConfirm field
     this.passwordConfirm = undefined;
     next();
-})
+});
+
+userSchema.methods.correctPassword = async function (candidatePassword, userPassword) {
+    return await bcrypt.compare(candidatePassword, userPassword)
+}
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
