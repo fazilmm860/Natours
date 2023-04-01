e4ba26b1aeb45ce737a0cf0b176779462e0a244e
Mohamed Fazil M <fazilmm860@gmail.com>
2023-04-01 15:43:09 +0530

    Updating the Currrent User:Data

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,8 +10,12 @@ router.post('/signup', authController.signup)
 router.post('/login', authController.login)
 
 router.post('/forgotPassword', authController.forgotPassword)
+
 router.patch('/resetPassword/:token', authController.resetPassword)
+
 router.patch('/updateMyPassword', authController.protect, authController.updatePassword)
+
+router.patch('/updateMe', authController.protect, userController.UpdateMe)
 router
     .route('/')
     .get(userController.getAllUsers)
