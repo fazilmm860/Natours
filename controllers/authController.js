98d64bac1e8da52ea7895b1bd95dc1141f0b1ff9
Mohamed Fazil M <fazilmm860@gmail.com>
2023-04-02 06:52:09 +0530

    Sending JWT via Cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,6 @@ const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel')
 const catchAsync = require('./../utils/catchAsync');
-const AppError = require('./../utils/appError');
 const sendEmail = require('./../utils/email');
 
 
@@ -14,6 +13,16 @@ const signToken = id => {
 }
 const createSendToken = (user, statusCode, res) => {
     const token = signToken(user.id)
+    const cookieOptions = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+        httpOnly: true
+    };
+    if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+    res.cookie('jwt', token, cookieOptions)
+
+    //Remove password from output
+    user.password = undefined
 
     res.status(statusCode).json({
         status: 'success',
