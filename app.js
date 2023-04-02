7c5a1f736a84aa3cbf9adf6716ec99ea1e7ce726
Mohamed Fazil M <fazilmm860@gmail.com>
2023-04-02 22:30:49 +0530

    Data Sanitization

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit')
 const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -33,6 +35,11 @@ app.use('/api', limiter);
 
 //Body parser,reading data from body into req.body
 app.use(express.json({ limit: "10kb" }));
+
+//Data sanitization against Nosql query injection
+app.use(mongoSanitize());
+//Data sanitization against XSS
+app.use(xss());
 
 //Serving static files
 app.use(express.static(`${__dirname}/public`));
